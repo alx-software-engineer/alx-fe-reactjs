cd8f85bbdb7c5e78f976420ca03f234c75c954cf
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-07-20 18:38:47 +0100

    Create Counter component in new react-app

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './Header'
 import MainContent from './MainContent'
 import Footer from './Footer'
 import WelcomeMessage from './WelcomeMessage'
+import Counter from './components/Counter'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -38,6 +39,7 @@ function App() {
       <MainContent /> 
       <Footer />
       <UserProfile name= "Alice" age= {25} bio= "Loves hiking and photography" />
+      <Counter />
     </>
   )
 }
