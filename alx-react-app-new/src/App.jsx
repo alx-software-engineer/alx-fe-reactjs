b201706a7b34e1a788a4869b9b9d7d1c5bd09685
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-07-20 18:53:10 +0100

    Remove unwanted file in App.jsx, in new react-app

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,8 +1,8 @@
-import UserProfile from './UserProfile'
-import Header from './Header'
-import MainContent from './MainContent'
-import Footer from './Footer'
-import WelcomeMessage from './WelcomeMessage'
+import UserProfile from './components/UserProfile'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import WelcomeMessage from './components/WelcomeMessage'
 import Counter from './components/Counter'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
@@ -14,26 +14,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <WelcomeMessage />
       <Header /> 
       <MainContent /> 
