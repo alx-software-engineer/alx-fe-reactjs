c9559e50173c3a89e92c61931cb5e4d150a911ca
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-07-21 11:53:45 +0100

    Added style

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -5,24 +5,28 @@ function Navbar() {
         <>
              <nav>
             <ul style={{
-                listStyleType: 'disc',
+                backgroundColor: "Gold",
+                display: "flex",
+                gap: "10px",
+                justifyContent: "center",
+                listStyleType: 'none',
                 paddingLeft: '20px',
                 margin: '10px 0',
             }}>
-                <li>
-                    <Link to="/">Home</Link>
+                <li style={{fontSize: '16px' }}>
+                    <Link to="/" style={{color: 'white'}}>Home</Link>
                 </li>
 
                  <li>
-                    <Link to="/about">About Us</Link>
+                    <Link to="/about" style={{color: 'white'}} >About Us</Link>
                 </li>
 
                  <li>
-                    <Link to="/services">Services</Link>
+                    <Link to="/services" style={{color: 'white'}} >Services</Link>
                 </li>
 
                  <li>
-                    <Link to="/contact">Contact Us</Link>
+                    <Link to="/contact" style={{color: 'white'}} >Contact Us</Link>
                 </li>
             </ul>
         </nav>
