a352ffebc2eaedaaa71813476b4a4b2728878c50
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-07-21 08:22:20 +0100

    Delete un-nesessary file in app.tsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,14 +4,9 @@ import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import WelcomeMessage from './components/WelcomeMessage'
 import Counter from './components/Counter'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <WelcomeMessage />
