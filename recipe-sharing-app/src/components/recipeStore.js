02f7b29e47a948bb72b01dd7e9610908cfa7c4fa
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-08-03 07:44:01 +0100

    Added recipeDetails

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,7 +3,13 @@ import { create } from 'zustand'
 const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+  
+  setRecipes: (recipes) => set({ recipes }),
+
+  deleteRecipe : (id) => set((state) => ({recipes : state.recipes.filter(recipe => recipe.id !== id)}))
+
+  
+
 }));
 
 export default useRecipeStore;
