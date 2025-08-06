d4ec1d7257c273cf508e88300ee3708bd8b4a7ea
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-08-06 12:32:24 +0100

    Create a search recipe feature

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,6 +2,15 @@ import { create } from 'zustand'
 
 const useRecipeStore = create(set => ({
   recipes: [],
+
+  filteredRecipes: [],
+
+  searchTerm: '',
+
+  setSearchTerm: (term) => set({ searchTerm: term }),
+
+  filterRecipes: () => set(state => ({filteredRecipes: state.recipes.filter(recipe => recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase()))})),
+
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   
   setRecipes: (recipes) => set({ recipes }),
