09f271b4b837f893cf28bebaf4510f7baea27c4f
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-08-16 17:17:21 +0100

    Added tailwind

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1 className='text-red-600'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
