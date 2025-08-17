f11840577dfb7f2ffe8b74ca17abef47e69fc3e8
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-08-17 07:36:41 +0100

    Reafctor folder structure

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,25 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-red-600'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='text-amber-300'>Vite + React</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, incidunt?</p>
     </>
   )
 }
