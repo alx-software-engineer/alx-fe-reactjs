0650da5378938e91085e5387d9df375cbdb10770
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-08-17 11:10:18 +0100

    Added Gihub services

--- github-user-search/src/components/Search.tsx
+++ github-user-search/src/components/Search.tsx
@@ -3,6 +3,20 @@ import { useState } from "react";
 function Search() {
     const [userName, setUserName] = useState("");
 
-    return 
+    return (
+        <form>
+            <div>
+                <input 
+                type="text"
+                name="username"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+                placeholder="Search UserName"
+                />
+            </div>
+        </form>
+    )
     
 }
+
+export default Search;
---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,4 @@
+// API Key and URL
+
+const apiUrl = import.meta.env.VITE_API_URL;
+const apiKey = import.meta.env.VITE_APP_GITHUB_API_KEY;
