7fa022e9df26151effa3a2daefd5f66c97b26d81
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-08-29 11:49:04 +0100

    Create Addrecipe form component

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -4,14 +4,14 @@ import { useState } from "react";
 function AddRecipeForm() {
     const [title, setTitle] = useState(null);
     const [ingredients, setIngredients] = useState(null);
-    const [instructions, setInstructions] = useState(null);
+    const [steps, setSteps] = useState(null);
 
 
 
 
     return (
         <form>
-
+            <h1>Hello World</h1>
         </form>
     )
 
